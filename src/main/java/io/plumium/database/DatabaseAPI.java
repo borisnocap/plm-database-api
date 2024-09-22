08f593aa773446d6299eb56f662b65e842275d5a
PC <@pc>
2024-09-22 18:40:43 +0300

    Удалены некоторые методы.

--- src/main/java/io/plumium/database/DatabaseAPI.java
+++ src/main/java/io/plumium/database/DatabaseAPI.java
@@ -14,23 +14,11 @@ public interface DatabaseAPI {
 
     Connection getConnection() throws SQLException;
 
-    Connection getConnectionWithoutAutoCommit() throws SQLException;
-
-    void rollbackCommit(Connection connection);
-
-    void close(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet);
-
-    void close(Connection connection, PreparedStatement preparedStatement);
-
     void close(Connection connection);
 
     void close(PreparedStatement preparedStatement);
 
     void close(ResultSet resultSet);
-
-    void closePool();
-
-    String getSqlQuery(PreparedStatement preparedStatement);
 
     /**
      * Получить ID, соответствующий указанному никнейму.
